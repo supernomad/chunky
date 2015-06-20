e41c81d08a4e27edb9a1a27de8c6c8e78dff4fa2
Supernomad <csaide@quantumnet.ninja>
2015-06-20 19:58:12 -0400

    removed some unused mocked calls

--- mocks/libs/caching/localCache.js
+++ mocks/libs/caching/localCache.js
@@ -2,19 +2,11 @@ var	typeHelper = require.main.require('libs/helpers/typeHelper'),
 	cache = {};
 function create(key, val, ttl, callback) {
 	cache[key] = val;
-	if(typeHelper.isFunction(callback)) {
-			callback(null, true);
-	} else {
-		return true;
-	}
+	callback(null, true);
 }
 
 function restore(key, callback) {
-	if(typeHelper.isFunction(callback)) {
-		callback(null, {key: key, value: cache[key]});
-	} else {
-		return {key: key, value: cache[key]};
-	}
+	callback(null, {key: key, value: cache[key]});
 }
 
 function update(key, val, ttl, callback) {
@@ -23,11 +15,7 @@ function update(key, val, ttl, callback) {
 
 function del(key, callback) {
 	delete cache[key];
-	if(typeHelper.isFunction(callback)) {
-		callback(null, 1);
-	} else {
-		return 1;
-	}
+	callback(null, 1);
 }
 
 module.exports = {
--- mocks/libs/io.js
+++ mocks/libs/io.js
@@ -1,11 +1,6 @@
 /* global Buffer */
 var fs = require('fs');
 
-function getFileStats(path, callback) {
-	// TODO get temp stats object
-	callback(null, {});
-}
-
 function createFile(path, buffer, offset, length, callback) {
 	callback(null);
 }
@@ -22,20 +17,14 @@ function readFile(path, callback) {
 	callback(null, new Buffer(0));
 }
 
-function readFileChunk(path, buffer, offset, length, position, callback) {	
-	callback(null, 0, new Buffer(0));
-}
-
 function renameFile(path, newPath, callback) {
 	callback(null);
 }
 
 module.exports = {
-	GetFileStats: getFileStats,
 	CreateFile: createFile,
 	WriteFileChunk: writeFileChunk,
 	DeleteFile: deleteFile,
 	ReadFile: readFile,
-	ReadFileChunk: readFileChunk,
 	RenameFile: renameFile
 };
